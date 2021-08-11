a64461bf40248ee7c59024c948a470a4ee4f30b9
David Taylor <david@taylorhq.com>
2021-08-11 18:47:30 +0100

    Add a zlist of all channels, and add scheduled auto_leave sidekiq job

--- lib/presence_channel.rb
+++ lib/presence_channel.rb
@@ -16,7 +16,7 @@ class PresenceChannel
     Discourse.redis.eval(
       ENTER_LUA,
       redis_keys,
-      [user_id, client_id, timeout]
+      [name, user_id, client_id, (Time.zone.now + timeout).to_i]
     )
 
     message = {
@@ -33,7 +33,7 @@ class PresenceChannel
     Discourse.redis.eval(
       LEAVE_LUA,
       redis_keys,
-      [user_id, client_id]
+      [name, user_id, client_id]
     )
 
     auto_leave
@@ -62,7 +62,7 @@ class PresenceChannel
     left_user_ids = Discourse.redis.eval(
       AUTO_LEAVE_LUA,
       redis_keys,
-      [Time.zone.now.to_i]
+      [name, Time.zone.now.to_i]
     )
 
     left_user_ids.each do |user_id|
@@ -76,58 +76,101 @@ class PresenceChannel
 
   end
 
+  # Clear all members of the channel. This is intended for debugging/development only
   def clear
     Discourse.redis.without_namespace.del(redis_key_zlist)
     Discourse.redis.without_namespace.del(redis_key_hash)
+    Discourse.redis.without_namespace.zrem(self.class.redis_key_channel_list, name)
   end
 
   private
 
-  def local_time
-    PresenceChannel.use_system_time ? Time.zone.now.to_i : nil
-  end
-
   def redis_keys
-    [redis_key_zlist, redis_key_hash]
+    [redis_key_zlist, redis_key_hash, self.class.redis_key_channel_list]
   end
 
+  # The zlist is a list of client_ids, ranked by their expiration timestamp
+  # we periodically delete the 'lowest ranked' items in this list based on the `timeout` of the channel
   def redis_key_zlist
     Discourse.redis.namespace_key("_presence_#{name}_zlist")
   end
 
+  # The hash contains a map of user_id => session_count
+  # when the count for a user reaches 0, the key is deleted
+  # We use this hash to efficiently count the number of present users
   def redis_key_hash
     Discourse.redis.namespace_key("_presence_#{name}_hash")
   end
 
+  # This list contains all active presence channels, ranked with the expiration timestamp of their least-recently-seen  client_id
+  # We periodically check the 'lowest ranked' items in this list based on the `timeout` of the channel
+  def self.redis_key_channel_list
+    Discourse.redis.namespace_key("_presence_channels")
+  end
+
+  # Designed to be run periodically. Checks the channel list for channels with expired members,
+  # and runs auto_leave for each eligable channel
+  def self.auto_leave_all
+    channels_with_expiring_members = Discourse.redis.zrangebyscore("_presence_channels", '-inf', Time.zone.now.to_i)
+    channels_with_expiring_members.each do |name|
+      new(name).auto_leave
+    end
+  end
+
+  # Clear all known channels. This is intended for debugging/development only
+  def self.clear_all!
+    channels = Discourse.redis.without_namespace.zrangebyscore(redis_key_channel_list, '-inf', '+inf')
+    channels.each do |name|
+      new(name).clear
+    end
+  end
+
   PARAMS_LUA = <<~LUA
-    local user_id = ARGV[1]
-    local client_id = ARGV[2]
-    local timeout = ARGV[3]
+    local channel = ARGV[1]
+    local user_id = ARGV[2]
+    local client_id = ARGV[3]
+    local expires = ARGV[4]
 
     local zlist_key = KEYS[1]
     local hash_key = KEYS[2]
+    local channels_key = KEYS[3]
+
+    local zlist_elem = tostring(user_id) .. " " .. tostring(client_id)
+  LUA
+
+  UPDATE_GLOBAL_CHANNELS_LUA = <<~LUA
+    -- Update the global channels list with the timestamp of the oldest client
+    local oldest_client = redis.call('ZRANGE', zlist_key, 0, 0, 'WITHSCORES')
+    if table.getn(oldest_client) > 0 then
+      local oldest_client_expire_timestamp = oldest_client[2]
+      redis.call('ZADD', channels_key, tonumber(oldest_client_expire_timestamp), tostring(channel))
+    else
+      -- The channel is now empty, delete from global list
+      redis.call('ZREM', channels_key, tostring(channel))
+    end
   LUA
 
   ENTER_LUA = <<~LUA
     #{PARAMS_LUA}
 
-    local now_raw = redis.call('TIME')
-    local now = tonumber(now_raw[1])
-
-    local zlist_elem = tostring(user_id) .. " " .. tostring(client_id)
-
-    redis.call('ZADD', zlist_key, now + timeout, zlist_elem)
+    redis.call('ZADD', zlist_key, expires, zlist_elem)
     redis.call('HINCRBY', hash_key, tostring(user_id), 1)
+
+    -- Add the channel to the global channel list. 'LT' means the value will
+    -- only be set if it's lower than the existing value
+    redis.call('ZADD', channels_key, "LT", expires, tostring(channel))
   LUA
 
   LEAVE_LUA = <<~LUA
     #{PARAMS_LUA}
 
-    local zlist_elem = tostring(user_id) .. " " .. tostring(client_id)
+    -- Remove the user from the channel zlist
+    local removed_count = tonumber(redis.call('ZREM', zlist_key, zlist_elem))
 
-    local position = tonumber(redis.call('ZREM', zlist_key, zlist_elem))
+    if removed_count > 0 then
+      #{UPDATE_GLOBAL_CHANNELS_LUA}
 
-    if tonumber(position) > 0 then
+      -- Update the user session count in the channel hash
       local val = redis.call('HINCRBY', hash_key, user_id, -1)
       if val <= 0 then
         redis.call('HDEL', hash_key, user_id)
@@ -157,9 +200,11 @@ class PresenceChannel
   AUTO_LEAVE_LUA = <<~LUA
     local zlist_key = KEYS[1]
     local hash_key = KEYS[2]
-    local time = ARGV[1]
+    local channels_key = KEYS[3]
+    local channel = ARGV[1]
+    local time = ARGV[2]
 
-    local expire = redis.call('ZRANGE', zlist_key, 0, time, 'BYSCORE')
+    local expire = redis.call('ZRANGE', zlist_key, '-inf', time, 'BYSCORE')
 
     local expired_user_ids = {}
 
@@ -175,6 +220,8 @@ class PresenceChannel
     table.foreach(expire, expireOld)
 
     redis.call('ZREMRANGEBYSCORE', zlist_key, "-inf", time)
+
+    #{UPDATE_GLOBAL_CHANNELS_LUA}
 
     return expired_user_ids
   LUA
--- spec/components/presence_channel_spec.rb
+++ spec/components/presence_channel_spec.rb
@@ -4,13 +4,14 @@ require 'rails_helper'
 require 'presence_channel'
 
 describe PresenceChannel do
+  before { PresenceChannel.clear_all! }
+  after { PresenceChannel.clear_all! }
+
   it "can perform basic channel functionality" do
     # 10ms timeout for testing
     channel1 = PresenceChannel.new("test")
     channel2 = PresenceChannel.new("test")
     channel3 = PresenceChannel.new("test")
-
-    channel1.clear
 
     expect(channel3.user_ids).to eq([])
 
@@ -34,7 +35,6 @@ describe PresenceChannel do
   it "can automatically expire users" do
 
     channel = PresenceChannel.new("test")
-    channel.clear
 
     channel.enter(user_id: 1)
     channel.enter(user_id: 1, client_id: 77)
@@ -48,7 +48,6 @@ describe PresenceChannel do
 
   it "correctly sends messages to message bus" do
     channel = PresenceChannel.new("test")
-    channel.clear
 
     messages = MessageBus.track_publish(channel.message_bus_channel_name) do
       channel.enter(user_id: 1, client_id: "a")
@@ -73,6 +72,34 @@ describe PresenceChannel do
       "user_id" => 1,
     }
     expect(messages[0].data).to eq(expected)
+  end
+
+  it "can track active channels, and auto_leave_all successfully" do
+    channel1 = PresenceChannel.new("test1")
+    channel2 = PresenceChannel.new("test2")
+
+    channel1.enter(user_id: 1, client_id: "a")
+    channel2.enter(user_id: 1, client_id: "a")
+
+    start_time = Time.zone.now
+
+    freeze_time start_time + PresenceChannel::DEFAULT_TIMEOUT / 2
+
+    channel2.enter(user_id: 2, client_id: "b")
+
+    freeze_time start_time + PresenceChannel::DEFAULT_TIMEOUT + 1
+
+    messages = MessageBus.track_publish do
+      PresenceChannel.auto_leave_all
+    end
+
+    expect(messages.map { |m| [ m.channel, m.data ] }).to contain_exactly(
+      ["/presence/test1", { "type" => "leave", "user_id" => 1 }],
+      ["/presence/test2", { "type" => "leave", "user_id" => 1 }]
+    )
+
+    expect(channel1.user_ids).to eq([])
+    expect(channel2.user_ids).to eq([2])
   end
 
 end
